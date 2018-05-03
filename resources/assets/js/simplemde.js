f47634b0a768a3073ef764473407e38f1334fc16
Tanyi Tibor <tibko0510@gmail.com>
2018-05-03 20:44:36 +0200

    Exported simplemde setting to editoConfig variable so it can be modified in the webpage.

--- resources/assets/js/simplemde.js
+++ resources/assets/js/simplemde.js
@@ -1,11 +1,13 @@
 import SimpleMDE from 'simplemde'
 
+window['editorConfig'] = {
+  element: document.querySelector('textarea.simple-mde'),
+  spellChecker: false,
+  promptURLs: true
+}
+
 window['initEditor'] = function () {
   /* eslint-disable no-new */
-  new SimpleMDE({
-    element: document.querySelector('textarea.simple-mde'),
-    spellChecker: false,
-    promptURLs: true
-  })
+  new SimpleMDE(editorConfig)
   /* eslint-enable no-new */
 }
